1f18ee967853763c9e4c9f5e5710e34f57b6d1d4
Dan Brooking <dan@rollipoll.com>
2016-02-04 11:31:05 -0500

    Contact page styled and data correct.

--- app/components/Contact.js
+++ app/components/Contact.js
@@ -1,14 +1,46 @@
 import React from 'react'
+import FontAwesome from 'react-fontawesome'
 
 export default class Contact extends React.Component {
-  render() {
+  render () {
     return (
-<div className="background-gray">
-  <div id="contact" className="container">
-    <h2>Contact</h2>
-
-</div>			</div>
-
-)
-}
+      <div className='background-blue'>
+        <div id='contact' className='container'>
+          <h2>Contact</h2>
+          <p className='lead'>
+            &ldquo;If I had asked people what they wanted, they would have said faster horses. &rdquo;<br />- Henry Ford
+          </p>
+          <hr />
+          <div className='row'>
+            <div className='col-lg-12'>
+              <ul className='no-bullets'>
+                <li>
+                  <a href='http://twitter.com/dbrooking' target='_blank'>
+                    <span><FontAwesome name='twitter'/></span>
+                    &nbsp;&nbsp;
+                    http://twitter.com/dbrooking
+                  </a>
+                </li>
+                <li>
+                  <a href='https://www.linkedin.com/in/danbrooking' target='_blank'>
+                    <span><FontAwesome name='linkedin'/></span>
+                    &nbsp;&nbsp;
+                    https://www.linkedin.com/in/danbrooking
+                  </a>
+                </li>
+                <li>
+                  <a href='mailto:dmbrooking@gmail.com'>
+                    <span><FontAwesome name='envelope' /></span>
+                    &nbsp;&nbsp;
+                    dmbrooking@gmail.com
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <hr />
+        </div>
+      </div>
+    )
+  }
 }
